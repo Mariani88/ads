7f2d001b4568aaf5398b6b1ed1f7de2b3ac467a8
matias.mariani <matias.mariani@etermax.com>
2018-10-04 13:37:13 -0300

    se agrega configuracion inicial de ads

--- app/src/main/java/com/example/matias/example/MainActivity.kt
+++ app/src/main/java/com/example/matias/example/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.matias.example
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713")
     }
 }
